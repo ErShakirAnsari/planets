562389da354312a7e435e9683ff2bc1e426e9d9e
Shakir Ansari <shakir.ansari@live.com>
2022-09-30 00:02:54 +0530

    added planet - neptune

---
+++ neptune/src/main/java/com/github/ershakiransari/planets/Neptune.java
@@ -0,0 +1,14 @@
+package com.github.ershakiransari.planets;
+
+/**
+ * @author Shakir
+ * @version 2022-09-29
+ */
+public class Neptune
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Neptune.class.getName() + " - 08");
+		System.out.println("v" + Neptune.class.getPackage().getImplementationVersion());
+	}
+}
