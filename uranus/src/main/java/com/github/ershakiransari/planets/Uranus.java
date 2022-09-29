cedf425b4c37b29e27c78060423e18e5242bee7f
Shakir Ansari <shakir.ansari@live.com>
2022-09-30 00:00:25 +0530

    added planet - uranus

---
+++ uranus/src/main/java/com/github/ershakiransari/planets/Uranus.java
@@ -0,0 +1,14 @@
+package com.github.ershakiransari.planets;
+
+/**
+ * @author Shakir
+ * @version 2022-09-29
+ */
+public class Uranus
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Uranus.class.getName() + " - 07");
+		System.out.println("v" + Uranus.class.getPackage().getImplementationVersion());
+	}
+}
