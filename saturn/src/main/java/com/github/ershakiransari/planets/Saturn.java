45d41fff11c301966882c6c0a5f07e273d108618
Shakir Ansari <shakir.ansari@live.com>
2022-09-29 23:56:46 +0530

    added planet - Saturn

---
+++ saturn/src/main/java/com/github/ershakiransari/planets/Saturn.java
@@ -0,0 +1,14 @@
+package com.github.ershakiransari.planets;
+
+/**
+ * @author Shakir
+ * @version 2022-09-29
+ */
+public class Saturn
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Saturn.class.getName() + " - 06");
+		System.out.println("v" + Saturn.class.getPackage().getImplementationVersion());
+	}
+}
