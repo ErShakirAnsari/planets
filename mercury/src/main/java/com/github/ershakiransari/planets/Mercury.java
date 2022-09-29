8b102562c6f3795daab687026113fca8fdd78519
Shakir Ansari <shakir.ansari@live.com>
2022-09-29 23:40:38 +0530

    added planet - mercury

---
+++ mercury/src/main/java/com/github/ershakiransari/planets/Mercury.java
@@ -0,0 +1,14 @@
+package com.github.ershakiransari.planets;
+
+/**
+ * @author Shakir
+ * @version 2022-09-29
+ */
+public class Mercury
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Mercury.class.getName() + " - 01");
+		System.out.println("v" + Mercury.class.getPackage().getImplementationVersion());
+	}
+}
