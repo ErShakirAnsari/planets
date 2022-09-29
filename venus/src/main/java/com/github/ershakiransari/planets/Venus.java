f5191a71dd677da19198ae427f3fee68e476e5c5
Shakir Ansari <shakir.ansari@live.com>
2022-09-29 23:45:59 +0530

    added planet - venus

---
+++ venus/src/main/java/com/github/ershakiransari/planets/Venus.java
@@ -0,0 +1,14 @@
+package com.github.ershakiransari.planets;
+
+/**
+ * @author Shakir
+ * @version 2022-09-29
+ */
+public class Venus
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Venus.class.getName() + " - 02");
+		System.out.println("v" + Venus.class.getPackage().getImplementationVersion());
+	}
+}
