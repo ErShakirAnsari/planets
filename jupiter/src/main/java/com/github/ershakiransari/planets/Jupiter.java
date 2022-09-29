de9f36c390a99c8e6a1ff72ec5befc4a7cfb73bf
Shakir Ansari <shakir.ansari@live.com>
2022-09-29 23:52:56 +0530

    added module - jupiter

---
+++ jupiter/src/main/java/com/github/ershakiransari/planets/Jupiter.java
@@ -0,0 +1,14 @@
+package com.github.ershakiransari.planets;
+
+/**
+ * @author Shakir
+ * @version 2022-09-29
+ */
+public class Jupiter
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Jupiter.class.getName() + " - 05");
+		System.out.println("v" + Jupiter.class.getPackage().getImplementationVersion());
+	}
+}
