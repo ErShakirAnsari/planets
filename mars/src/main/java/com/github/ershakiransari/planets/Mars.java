44288f19c6de372eadbdc91dc141b4a5b4ba16d1
Shakir Ansari <shakir.ansari@live.com>
2022-09-29 23:50:41 +0530

    added planet - mars

---
+++ mars/src/main/java/com/github/ershakiransari/planets/Mars.java
@@ -0,0 +1,14 @@
+package com.github.ershakiransari.planets;
+
+/**
+ * @author Shakir
+ * @version 2022-09-29
+ */
+public class Mars
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Mars.class.getName() + " - 04");
+		System.out.println("v" + Mars.class.getPackage().getImplementationVersion());
+	}
+}
