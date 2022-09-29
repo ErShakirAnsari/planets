b361d7573419bc8a4c6210ec7e9dfe8d84d49336
Shakir Ansari <shakir.ansari@live.com>
2022-09-29 23:33:00 +0530

    added planet - earth

---
+++ earth/src/main/java/com/github/ershakiransari/planets/Earth.java
@@ -0,0 +1,14 @@
+package com.github.ershakiransari.planets;
+
+/**
+ * @author Shakir
+ * @version 2022-09-29
+ */
+public class Earth
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Earth.class.getName());
+		System.out.println("v" + Earth.class.getPackage().getImplementationVersion());
+	}
+}
